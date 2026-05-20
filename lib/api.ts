364e056150bb635bdca58ad15c3538178e75a835
arifnotify <arifnotify@gmail.com>
2026-05-20 15:23:49 +0300

    update

--- lib/api.ts
+++ lib/api.ts
@@ -6,17 +6,3 @@ const api = axios.create({
 
 export default api;
 
-export const getCategories = async () => {
-  const res = await api.get('/categories');
-  return res.data;
-};
-
-export const createCategory = async (data: any, token: string) => {
-  const res = await api.post('/categories', data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  return res.data;
-};
