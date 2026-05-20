67da7f1bb4017236f303f0c116998a68dd68ecc7
arifnotify <arifnotify@gmail.com>
2026-05-20 15:30:47 +0300

    update 1

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ecommerce-backend-njlc.onrender.com',
+  baseURL: 'https://ecommerce-backend-njlc.onrender.com/',
 });
 
 export default api;
